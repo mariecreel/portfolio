174c7386ef1f2afd830ac591189ba94e5a0fa4cb
mcreel <nick.creel@protonmail.com>
2021-02-10 12:54:08 -0500

    implemented project carousel, removed project cards, added contacts

--- portfolio-site/src/App.js
+++ portfolio-site/src/App.js
@@ -4,29 +4,42 @@ import ProjectCards from './ProjectCards.js'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header container-fluid">
       <h1 className="title">M. Creel</h1>
       <h2 className="subtitle">Front-end developer, 42.3464° N, 71.1627° W.</h2>
       </header>
-      <div className="overview">
+      <div className="overview container-fluid">
         <h1 className="overview-title">Overview</h1>
-        <p className="overview-content">I've always been obsessed with computers
-        and the possibilities they offer for creating. As a teen, I worked deep
-        into the night tweaking the CSS on my blog, motivated by how wonderful
-        it felt to express myself through code. It only seemed natural to pursue
-        computer science in college, as I had only grown more curious about
-        how computer code transformed from written word to interactive interfaces.
-        I gained valuable experience with web development, becoming
-        familiar with tools such as Javascript, Python, Node.js, and React, as
-        well as broader concepts such as mobile-first design, web accessibility,
-        UI/UX, and developing within a team.I know I have much to learn still,
-        but I'm determined to hone my skills by doing, developing
-        and designing beautiful, functional, user-friendly webpages.
-        </p>
+        <div className="overview-content row justify-content-center">
+          <div className="col justify-content-center">
+            I've always been obsessed with computers
+            and the possibilities they offer for creating. As a teen, I worked deep
+            into the night tweaking the CSS on my blog, motivated by how wonderful
+            it felt to express myself through code. It seemed natural to pursue
+            computer science in college, as I grew more curious about
+            how computer code transformed from what I typed to what I saw on the screen.
+            I gained valuable experience with web development, becoming
+            familiar with tools such as Javascript, Python, Node.js, and React, as
+            well as broader concepts such as mobile-first design, web accessibility,
+            UI/UX, and developing within a team.I know I have much to learn still,
+            but I'm determined to hone my skills by doing, developing
+            and designing beautiful, functional, user-friendly webpages.
+          </div>
+        </div>
       </div>
-      <div className="projects">
+      <div className="projects container-fluid">
         <h1 className="projects-title">Projects</h1>
         <ProjectCards/>
+      </div>
+      <div className="contact container-fluid">
+        <div className="contact-title">Contact</div>
+        <div className="contact-content">
+          <div className="row justify-content-center">
+              <div className="col-2"><a href="https://github.com/ncreel" target="_blank">Github</a></div>
+              <div className="col-2"><a href="https://linkedin.com/in/m-creel" target="_blank">Linkedin</a></div>
+              <div className="col-2"><a href="mailto:nick.creel@pm.me">nick.creel@pm.me</a></div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- portfolio-site/src/ProjectCards.js
+++ portfolio-site/src/ProjectCards.js
@@ -1,27 +1,57 @@
 import React from 'react';
-
+import gamez4grrrlz from './gamez4grrlz.png';
+import hatsunemicu from './hatsunemicu.png';
+import moviesearch from './moviesearch.png';
 
 export default function ProjectCards(){
   return (
-    <div className="content">
-      <div className="card">
-        <h3 className="card--title">
-          <a href="https://cursed.co.in/movie-db-react">Movie Search
-          </a>
-        </h3>
+    <div id="projects" className="carousel slide" data-ride="carousel">
+      <div className="carousel-inner">
+
+
+        <div className="carousel-item active">
+          <img className="carousel-image"src={moviesearch} alt="Movie Search"/>
+          <div className="overlay">
+            <p className="overlay-text">
+              <a href="https://cursed.co.in/movie-db-react" target="_blank"><h2>Movie Search</h2></a><br/>
+              Simple movie search engine powers by The Movie Database's API (TMDb API).<br/><br/>
+              Software used: Javascript, React, HTML/CSS
+            </p>
+          </div>
+        </div>
+
+        <div className="carousel-item">
+          <img className="carousel-image"src={gamez4grrrlz} alt="gamez4grrrlz"/>
+          <div className="overlay">
+            <p className="overlay-text">
+              <a href="https://cursed.co.in/gamez4grrrlz" target="_blank"><h2>gamez4grrrlz</h2></a><br/>
+              Collection of three interactive hypertext poems inspired by classic flash games. <br/><br/>
+              Software used: Javascript, HTML/CSS, JQueryUI
+            </p>
+          </div>
+        </div>
+
+        <div className="carousel-item">
+          <img className="carousel-image"src={hatsunemicu} alt="hatsunem.icu"/>
+          <div className="overlay">
+            <p className="overlay-text">
+              <a href="" target="_blank"><h2>hatsunem.icu</h2></a><br/> Loads a random youtube video from a curated playlist of
+              songs featuring Vocaloid Hatsune Miku. <br/><br/>
+              Software used: Python, Flask, Youtube API, HTML/CSS, gunicorn, Heroku
+            </p>
+          </div>
+        </div>
+
+
       </div>
-      <div className="card">
-        <h3 className="card--title">
-          <a href="https://cursed.co.in/gamez4grrrlz">gamez4grrrlz
-          </a>
-        </h3>
-      </div>
-      <div className="card">
-        <h3 className="card--title">
-          <a href="http://hatsunem.icu">hatsunem.icu
-          </a>
-        </h3>
-      </div>
+      <a class="carousel-control-prev" href="#projects" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#projects" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
     </div>
   )
 }
