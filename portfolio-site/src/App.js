57dd3f43e5855cebd2581c8fb8461b45f5f1be92
mcreel <nick.creel@protonmail.com>
2021-02-09 20:14:57 -0500

    removed default images/code from create-react-app, styled header, created ProjectCards.js

--- portfolio-site/src/App.js
+++ portfolio-site/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import ProjectCards from './ProjectCards.js'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1 className="title">Marie Creel</h1>
+      <h2 className="subtitle">Front-end developer, 42.3464° N, 71.1627° W.</h2>
       </header>
+      <div className="projects">
+        <h1 className="subtitle">Projects</h1>
+        <ProjectCards/>
+      </div>
     </div>
   );
 }
---
+++ portfolio-site/src/ProjectCards.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+export default function ProjectCards(){
+  return (
+    <div className="content">
+      <div className="card">
+        <h3 className="card--title">Movie Search</h3>
+
+      </div>
+      <div className="card">
+        <h3 className="card--title">hatsunem.icu</h3>
+
+      </div>
+    </div>
+  )
+}
