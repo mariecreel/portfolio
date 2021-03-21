81fe1ddf5b4858f4c9d29d470d7d8b3a076297ff
ncreel <nick.creel@protonmail.com>
2021-03-21 19:55:42 -0400

    add links to carousel text for each card pointing to API's used

--- portfolio-site/src/ProjectCards.js
+++ portfolio-site/src/ProjectCards.js
@@ -14,7 +14,7 @@ export default function ProjectCards(){
           <div className="overlay">
             <p className="overlay-text">
               <a href="https://cursed.co.in/movie-db-react" target="_blank"><h2>Movie Search</h2></a><br/>
-              Simple movie search engine powers by The Movie Database's API (TMDb API).<br/><br/>
+              Simple movie search engine powers by <a href="https://developers.themoviedb.org/3/getting-started/introduction">The Movie Database's API (TMDb API)</a>.<br/><br/>
               Software used: JavaScript, React, HTML/CSS
             </p>
           </div>
@@ -25,7 +25,7 @@ export default function ProjectCards(){
           <div className="overlay">
             <p className="overlay-text">
               <a href="https://cursed.co.in/gamez4grrrlz" target="_blank"><h2>MBTA Arrival Predictions (In Development)</h2></a><br/>
-              Predicts when the next train arrives at a selected stop on the MBTA subway system. <br/><br/>
+              Predicts when the next train arrives at a selected stop on the MBTA subway system through calls to the <a href="https://www.mbta.com/developers/v3-api">MBTA API</a>. <br/><br/>
               Software used: JavaScript, TypeScript, Svelte, HTML/CSS, Rollup, Vercel
             </p>
           </div>
@@ -36,8 +36,8 @@ export default function ProjectCards(){
           <div className="overlay">
             <p className="overlay-text">
               <a href="http://hatsunem.icu" target="_blank"><h2>hatsunem.icu</h2></a><br/> Loads a random youtube video from a curated playlist of
-              songs featuring Vocaloid Hatsune Miku. <br/><br/>
-              Software used: Python, Flask, Youtube API, HTML/CSS, gunicorn, Heroku
+              songs featuring Vocaloid Hatsune Miku via calls to the <a href="https://developers.google.com/youtube/v3">YouTube API<a/>.<br/><br/>
+              Software used: Python, Flask, HTML/CSS, gunicorn, Heroku
             </p>
           </div>
         </div>
