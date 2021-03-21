bd22d39df7d1c1fea61f4565f63a6a7578e72942
ncreel <nick.creel@protonmail.com>
2021-03-21 18:25:50 -0400

    change overview text

--- portfolio-site/src/App.js
+++ portfolio-site/src/App.js
@@ -12,7 +12,13 @@ function App() {
         <h1 className="overview-title">Overview</h1>
         <div className="overview-content row justify-content-center">
           <div className="col justify-content-center">
-
+          I'm a recent Computer Science and Writing graduate searching for
+          opportunities to develop software with social impact. My skills
+          lie mainly in front end development, specifically in Javascript, HTML,
+          and CSS. I've created websites (including this one!) using modern
+          Javascript technologies, such as React, Svelte, and Typescript, and
+          I have experience designing UI with Bootstrap and Sass. I'm eager to
+          learn new technologies and contribute to software with purpose.
           </div>
         </div>
       </div>
