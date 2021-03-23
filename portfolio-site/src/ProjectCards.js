7f0d47d0325dc36d2420c338f1cddb59b60d5706
ncreel <nick.creel@protonmail.com>
2021-03-23 09:09:44 -0400

    add links to title for each card

--- portfolio-site/src/ProjectCards.js
+++ portfolio-site/src/ProjectCards.js
@@ -13,7 +13,7 @@ export default function ProjectCards(){
           <img className="carousel-image"src={moviesearch} alt="Movie Search"/>
           <div className="overlay">
             <p className="overlay-text">
-              <a href="https://cursed.co.in/movie-db-react" target="_blank"><h2>Movie Search</h2></a><br/>
+              <a href="https://cursed.co.in/movie-db-react" target="_blank"><h2>Movie Search</h2></a>
               <a href="https://github.com/ncreel/movie-db-react/">Github Repo</a><br/>
               Searches for movies through calls to <a href="https://developers.themoviedb.org/3/getting-started/introduction">The Movie Database's API (TMDb API)</a>.<br/><br/>
               Software used: JavaScript, React, HTML/CSS
@@ -25,7 +25,7 @@ export default function ProjectCards(){
           <img className="carousel-image"src={mbtaarrivals} alt="mbtaarrivals"/>
           <div className="overlay">
             <p className="overlay-text">
-              <a href="https://mbta-app.vercel.app/" target="_blank"><h2>MBTA Arrival Predictions (In Development)</h2></a><br/>
+              <a href="https://mbta-app.vercel.app/" target="_blank"><h2>MBTA Arrival Predictions (In Development)</h2></a>
               <a href="https://github.com/ncreel/mbta-app">Github Repo</a><br/>
               Predicts when the next train arrives at a selected stop on the MBTA subway system through calls to the <a href="https://www.mbta.com/developers/v3-api">MBTA API</a>. <br/><br/>
               Software used: JavaScript, TypeScript, Svelte, HTML/CSS, Rollup, Vercel
@@ -37,7 +37,7 @@ export default function ProjectCards(){
           <img className="carousel-image"src={hatsunemicu} alt="hatsunem.icu"/>
           <div className="overlay">
             <p className="overlay-text">
-              <a href="http://hatsunem.icu" target="_blank"><h2>hatsunem.icu</h2></a><br/>
+              <a href="http://hatsunem.icu" target="_blank"><h2>hatsunem.icu</h2></a>
               <a href="https://github.com/ncreel/hatsunem.icu">Github Repo</a><br/>
               Loads a random youtube video from a curated playlist of
               songs featuring Vocaloid Hatsune Miku through calls to the <a href="https://developers.google.com/youtube/v3">YouTube API</a>.<br/><br/>
