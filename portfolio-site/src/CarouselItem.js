dbb206a54bbf8d1e536310ffaf8771de23e088ac
ncreel <nick.creel@protonmail.com>
2021-03-31 22:17:33 -0400

    fixed issue with active and inactive carousel panes

--- portfolio-site/src/CarouselItem.js
+++ portfolio-site/src/CarouselItem.js
@@ -3,18 +3,33 @@ import CarouselImage from './CarouselImage.js';
 import CarouselOverlay from './CarouselOverlay.js';
 
 export default function CarouselItem(props){
-  console.log(props)
-  return(
-    <div className="carousel-item active" key="{props.key}">
-      <CarouselImage src={props.image}
-                    alt={props.title}
-      />
-      <CarouselOverlay href={props.href}
-               title={props.title}
-               repo={props.repo}
-               headline={props.headline}
-               software={props.software}
-      />
-    </div>
-  )
+  if (props.active!=""){
+    return(
+      <div className="carousel-item active" key="{props.key}">
+        <CarouselImage src={props.image}
+                      alt={props.title}
+        />
+        <CarouselOverlay href={props.href}
+                 title={props.title}
+                 repo={props.repo}
+                 headline={props.headline}
+                 software={props.software}
+        />
+      </div>
+    )
+  } else {
+    return(
+      <div className="carousel-item" key="{props.key}">
+        <CarouselImage src={props.image}
+                      alt={props.title}
+        />
+        <CarouselOverlay href={props.href}
+                 title={props.title}
+                 repo={props.repo}
+                 headline={props.headline}
+                 software={props.software}
+        />
+      </div>
+    )
+  }
 }
